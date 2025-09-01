ea415c52a84730c3bd533240ce995fe853ea1a8c
FurkanYildirim07 <furkanyldrm1607@gmail.com>
2025-09-02 00:09:09 +0300

    code review is completed, some todos are added

--- example/Core/Inc/sensor.h
+++ example/Core/Inc/sensor.h
@@ -16,14 +16,14 @@
 #include <string.h>
 
 /* Exported constants --------------------------------------------------------*/
-#define AGS10MA_I2C_ADDR_DEFAULT   0x1A    /**< Default I2C address */
+#define AGS10MA_I2C_DEFAULT_ADDR   0x1A    /**< Default I2C address */
 #define AGS10MA_I2C_WRITE          0x34    /**< Write instruction */
 #define AGS10MA_I2C_READ           0x35    /**< Read instruction */
 
-#define AGS10MA_TVOCSTAT_REG       0x00    /**< Status and TVOC reading */
+#define AGS10MA_TVOC_STAT_REG      0x00    /**< Status and TVOC reading */
 #define AGS10MA_VERSION_REG        0x11    /**< Firmware version */
 #define AGS10MA_GASRES_REG         0x20    /**< Raw gas resistance */
-#define AGS10MA_SETADDR_REG        0x21    /**< Change I2C address */
+#define AGS10MA_SET_ADDR_REG       0x21    /**< Change I2C address */
 
 /* Exported types ------------------------------------------------------------*/
 typedef struct
@@ -39,7 +39,7 @@ typedef struct
  * @param  len  Length of data
  * @retval CRC8 value
  */
-uint8_t AGS10MA_CRC8(const uint8_t *data, int len);
+uint8_t ags10ma_crc8(const uint8_t *data, int len);
 
 /**
  * @brief  Initialize the AGS10 sensor
@@ -58,7 +58,7 @@ bool AGS10MA_Init(AGS10MA_HandleTypeDef *sensor, I2C_HandleTypeDef *hi2c, uint8_
  * @param  value  Pointer to store the read value
  * @retval true if read successful, false otherwise
  */
-bool AGS10MA_ReadRegister(AGS10MA_HandleTypeDef *sensor, uint8_t reg, uint16_t delayms, uint32_t *value);
+bool ags10ma_register_read(AGS10MA_HandleTypeDef *sensor, uint8_t reg, uint16_t delayms, uint32_t *value);
 
 /**
  * @brief  Get firmware version
@@ -66,7 +66,7 @@ bool AGS10MA_ReadRegister(AGS10MA_HandleTypeDef *sensor, uint8_t reg, uint16_t d
  * @param  version Pointer to store firmware version
  * @retval true if successful, false otherwise
  */
-bool AGS10MA_GetFirmwareVersion(AGS10MA_HandleTypeDef *sensor, uint32_t *version);
+bool ags10ma_firmware_version_get(AGS10MA_HandleTypeDef *sensor, uint32_t *version);
 
 /**
  * @brief  Get raw gas resistance
@@ -74,7 +74,7 @@ bool AGS10MA_GetFirmwareVersion(AGS10MA_HandleTypeDef *sensor, uint32_t *version
  * @param  resistance Pointer to store gas resistance
  * @retval true if successful, false otherwise
  */
-bool AGS10MA_GetGasResistance(AGS10MA_HandleTypeDef *sensor, uint32_t *resistance);
+bool ags10ma_gas_resistance_get(AGS10MA_HandleTypeDef *sensor, uint32_t *resistance);
 
 /**
  * @brief  Get TVOC value
@@ -82,7 +82,7 @@ bool AGS10MA_GetGasResistance(AGS10MA_HandleTypeDef *sensor, uint32_t *resistanc
  * @param  tvoc Pointer to store TVOC value
  * @retval true if successful, false otherwise
  */
-bool AGS10MA_GetTVOC(AGS10MA_HandleTypeDef *sensor, uint32_t *tvoc);
+bool ags10ma_tvOc_get(AGS10MA_HandleTypeDef *sensor, uint32_t *tvoc);
 
 /**
  * @brief  Set new I2C address for sensor
@@ -90,6 +90,6 @@ bool AGS10MA_GetTVOC(AGS10MA_HandleTypeDef *sensor, uint32_t *tvoc);
  * @param  new_addr New I2C address
  * @retval true if successful, false otherwise
  */
-bool AGS10MA_SetAddress(AGS10MA_HandleTypeDef *sensor, uint8_t new_addr);
+bool ags10ma_address_set(AGS10MA_HandleTypeDef *sensor, uint8_t new_addr);
 
 #endif /* INC_AGS10_GAS_SENSOR_H_ */
